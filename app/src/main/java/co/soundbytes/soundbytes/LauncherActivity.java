ca05cca20de6e6e52047fa9396bde2f8cdbb6692
Olumide Awofeso <olumideawofeso@gmail.com>
2015-10-03 12:22:48 -0400

    Added Navigation bar
    This is still just a skeletal layout of the main activity

--- app/src/main/java/co/soundbytes/soundbytes/LauncherActivity.java
+++ app/src/main/java/co/soundbytes/soundbytes/LauncherActivity.java
@@ -1,9 +1,11 @@
 package co.soundbytes.soundbytes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class LauncherActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchMainActivity(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/co/soundbytes/soundbytes/MainActivity.java
+++ app/src/main/java/co/soundbytes/soundbytes/MainActivity.java
@@ -1,16 +1,42 @@
 package co.soundbytes.soundbytes;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout)findViewById(R.id.main_activity_drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ){
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+//                getActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+//                getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+        mDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
