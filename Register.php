1dafe6384faa1bd9dfd2b7bcb7bf027c006e2e38
Naveen Ganessin <naveenganessin@gmail.com>
2015-10-29 00:00:41 -0400

    Register
    This PHP script basically helps the register activity in sending the
    user info to the database. It has also been recently modified to create
    a new table for each user that gets registered.

---
+++ Register.php
@@ -0,0 +1,26 @@
+<?php
+    $con=mysqli_connect("localhost","nganessi_tester","webhost94","nganessi_logintutorial");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "INSERT INTO `User` (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_close($statement);
+    
+    $statement2 = "CREATE TABLE $username(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        friendName VARCHAR(30) NOT NULL)";
+    
+    if($con->query($statement2) === TRUE){
+        echo "Table successfully created!";
+    } else {
+        echo "Error creating table: " .$con->error;
+    }
+
+    mysqli_close($con);
+?>
